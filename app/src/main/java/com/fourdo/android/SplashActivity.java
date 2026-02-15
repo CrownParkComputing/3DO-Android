d79276d0d614dfd8d0a57e021813fb64d3bd8e97
Jon Whittingham <jon@crownparkcomputing.com>
2026-02-15 22:10:36 +0000

    FIXRS AND GFX

---
+++ app/src/main/java/com/fourdo/android/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.fourdo.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends Activity {
+
+    private static final long SPLASH_DURATION_MS = 2000L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION_MS);
+    }
+}
